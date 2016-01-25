ab5c3ece3c29e729d27a0e31a33de22b2a93f99b
Vien Van <vienvonvan@gmail.com>
2016-01-25 11:11:12 -0800

    Added email-request prompt

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
 // javascript to be added later
+console.log("linked");
+var email = document.getElementById('button-yellow');
+
+email.addEventListener('click', function(e) {
+  prompt("Please enter your Email");
+})
